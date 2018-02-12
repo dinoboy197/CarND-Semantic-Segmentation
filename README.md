8a3c999247033c8b1572cd0a76af0f8c9599927a
Taylor Raack <taylor@raack.info>
2018-02-11 17:53:38 -0800

    Implement Semantic Segmentation Project

--- README.md
+++ README.md
@@ -1,46 +1,71 @@
 # Semantic Segmentation
-### Introduction
-In this project, you'll label the pixels of a road in images using a Fully Convolutional Network (FCN).
 
-### Setup
-##### Frameworks and Packages
+## Goal
+
+This project labels the pixels of a road in images using a Fully Convolutional Network (FCN).
+
+---
+
+## Example images classified
+
+[//]: # (Image References)
+[image1]: ./examples/sample1.png
+[image2]: ./examples/sample2.png
+[image3]: ./examples/sample3.png
+[image4]: ./examples/sample4.png
+
+![alt text][image1]
+
+![alt text][image2]
+
+![alt text][image3]
+
+![alt text][image4]
+
+All images classified from the test dataset can be seen [here](runs/1518933171.754386).
+
+## Implementation Details
+
+A modified version of the impressive VGG16 neural network image classification pipeline is used to classify road vs non-road pixels images. The program takes a pre-trained fully-convolutional network based on [FCN-8](https://github.com/shelhamer/fcn.berkeleyvision.org) and adds skip layers to allow for semantic segmentation of pixels in the image.
+
+From the originally inputted layer, the input, keep probability, and layers 3, 4, and 7 are [extracted for further use](https://github.com/dinoboy197/CarND-Semantic-Segmentation/blob/master/main.py#L21-L44).
+
+Next, [1x1 convolutions are constructed from layers 3, 4, and 7](https://github.com/dinoboy197/CarND-Semantic-Segmentation/blob/master/main.py#L65-L77) in an encoding step. Skip layers are inserted by adding the 1x1 convolutions from layers [3](https://github.com/dinoboy197/CarND-Semantic-Segmentation/blob/master/main.py#L84-L85) and [4](https://github.com/dinoboy197/CarND-Semantic-Segmentation/blob/master/main.py#L91-L92). Layers 3, 4, and 7 are [deconvolved in reverse order](https://github.com/dinoboy197/CarND-Semantic-Segmentation/blob/master/main.py#L79-L102) to complete the final piece of the decoding step.
+
+After the network architecture is created, an [optimizer is created](https://github.com/dinoboy197/CarND-Semantic-Segmentation/blob/master/main.py#L109-L131) to minimize the softmax cross-entropy between the logits created by the network and the correct labels for image pixels. An optimizer using the Adam method is used to compute gradients for loss and gradients to variables in the networks.
+
+The [neural network is then trained](https://github.com/dinoboy197/CarND-Semantic-Segmentation/blob/master/main.py#L136-L172) using a sample of labeled images for a maximum of 50 epochs. A mini-batch size of 10 images is used compromise between high memory footprint and smooth network convergence. The training step has an early terminator which does not continue to train the network if total training loss does not decrease for three subsequent epochs.
+
+Finally, a separate sample of test images are run through the final neural network classifier for evaluation.
+
+## Future improvements
+
+* Use [the Cityscapes dataset](https://www.cityscapes-dataset.com/) for more images to train a network that can classify more than simply road / non-road pixels.
+* Augment input images by flipping on the horizontal axis to improve network generalization.
+* Apply trained classifer to a video stream for fun!
+
+---
+
+## Running the project
+
+#### Frameworks and Packages
 Make sure you have the following is installed:
  - [Python 3](https://www.python.org/)
  - [TensorFlow](https://www.tensorflow.org/)
  - [NumPy](http://www.numpy.org/)
  - [SciPy](https://www.scipy.org/)
-##### Dataset
+
+#### Dataset
 Download the [Kitti Road dataset](http://www.cvlibs.net/datasets/kitti/eval_road.php) from [here](http://www.cvlibs.net/download.php?file=data_road.zip).  Extract the dataset in the `data` folder.  This will create the folder `data_road` with all the training a test images.
 
-### Start
-##### Implement
-Implement the code in the `main.py` module indicated by the "TODO" comments.
-The comments indicated with "OPTIONAL" tag are not required to complete.
-##### Run
+#### Run
 Run the following command to run the project:
 ```
 python main.py
 ```
-**Note** If running this in Jupyter Notebook system messages, such as those regarding test status, may appear in the terminal rather than the notebook.
 
-### Submission
-1. Ensure you've passed all the unit tests.
-2. Ensure you pass all points on [the rubric](https://review.udacity.com/#!/rubrics/989/view).
-3. Submit the following in a zip file.
- - `helper.py`
- - `main.py`
- - `project_tests.py`
- - Newest inference images from `runs` folder  (**all images from the most recent run**)
- 
- ### Tips
+#### Notes
 - The link for the frozen `VGG16` model is hardcoded into `helper.py`.  The model can be found [here](https://s3-us-west-1.amazonaws.com/udacity-selfdrivingcar/vgg.zip)
 - The model is not vanilla `VGG16`, but a fully convolutional version, which already contains the 1x1 convolutions to replace the fully connected layers. Please see this [forum post](https://discussions.udacity.com/t/here-is-some-advice-and-clarifications-about-the-semantic-segmentation-project/403100/8?u=subodh.malgonde) for more information.  A summary of additional points, follow. 
 - The original FCN-8s was trained in stages. The authors later uploaded a version that was trained all at once to their GitHub repo.  The version in the GitHub repo has one important difference: The outputs of pooling layers 3 and 4 are scaled before they are fed into the 1x1 convolutions.  As a result, some students have found that the model learns much better with the scaling layers included. The model may not converge substantially faster, but may reach a higher IoU and accuracy. 
 - When adding l2-regularization, setting a regularizer in the arguments of the `tf.layers` is not enough. Regularization loss terms must be manually added to your loss function. otherwise regularization is not implemented.
- 
-### Using GitHub and Creating Effective READMEs
-If you are unfamiliar with GitHub , Udacity has a brief [GitHub tutorial](http://blog.udacity.com/2015/06/a-beginners-git-github-tutorial.html) to get you started. Udacity also provides a more detailed free [course on git and GitHub](https://www.udacity.com/course/how-to-use-git-and-github--ud775).
-
-To learn about REAMDE files and Markdown, Udacity provides a free [course on READMEs](https://www.udacity.com/courses/ud777), as well. 
-
-GitHub also provides a [tutorial](https://guides.github.com/features/mastering-markdown/) about creating Markdown files.
--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import os.path
 import tensorflow as tf
 import helper
 import warnings
+import sys
+
 from distutils.version import LooseVersion
 import project_tests as tests
-
 
 # Check TensorFlow Version
 assert LooseVersion(tf.__version__) >= LooseVersion('1.0'), 'Please use TensorFlow version 1.0 or newer.  You are using {}'.format(tf.__version__)
@@ -24,16 +25,24 @@ def load_vgg(sess, vgg_path):
     :param vgg_path: Path to vgg folder, containing "variables/" and "saved_model.pb"
     :return: Tuple of Tensors from VGG model (image_input, keep_prob, layer3_out, layer4_out, layer7_out)
     """
-    # TODO: Implement function
-    #   Use tf.saved_model.loader.load to load the model and weights
     vgg_tag = 'vgg16'
     vgg_input_tensor_name = 'image_input:0'
     vgg_keep_prob_tensor_name = 'keep_prob:0'
     vgg_layer3_out_tensor_name = 'layer3_out:0'
     vgg_layer4_out_tensor_name = 'layer4_out:0'
     vgg_layer7_out_tensor_name = 'layer7_out:0'
-    
-    return None, None, None, None, None
+
+    # load saved model and access model graph
+    tf.saved_model.loader.load(sess, [vgg_tag], vgg_path)
+    graph = tf.get_default_graph()
+
+    # return useful tensors
+    return (graph.get_tensor_by_name(vgg_input_tensor_name),
+            graph.get_tensor_by_name(vgg_keep_prob_tensor_name),
+            graph.get_tensor_by_name(vgg_layer3_out_tensor_name),
+            graph.get_tensor_by_name(vgg_layer4_out_tensor_name),
+            graph.get_tensor_by_name(vgg_layer7_out_tensor_name))
+
 tests.test_load_vgg(load_vgg, tf)
 
 
@@ -46,8 +55,54 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     :param num_classes: Number of classes to classify
     :return: The Tensor for the last layer of output
     """
-    # TODO: Implement function
-    return None
+
+    # create a common initializer to use on all layers
+    initializer = tf.random_normal_initializer(stddev=0.01)
+
+    # create a common L2 regularlizer to use on all layers
+    regularizer = tf.contrib.layers.l2_regularizer(1e-3)
+
+    # ENCODING
+
+    # apply 1x1 convolution to layer 3 of pre-created network
+    layer_3_1x1_convolution = tf.layers.conv2d(vgg_layer3_out, num_classes, 1, padding='same',
+                                               kernel_initializer=initializer, kernel_regularizer=regularizer)
+
+    # apply 1x1 convolution to layer 4 of pre-created network
+    layer_4_1x1_convolution = tf.layers.conv2d(vgg_layer4_out, num_classes, 1, padding='same',
+                                               kernel_initializer=initializer, kernel_regularizer=regularizer)
+
+    # apply 1x1 convolution to layer 7 of pre-created network
+    layer_7_1x1_convolution = tf.layers.conv2d(vgg_layer7_out, num_classes, 1, padding='same',
+                                               kernel_initializer=initializer, kernel_regularizer=regularizer)
+
+    # DECODING
+
+    # apply deconvolution to output of layer 7 1x1 convolution
+    layer_7_deconvolution = tf.layers.conv2d_transpose(layer_7_1x1_convolution, num_classes, 4, 2, padding='same',
+                                                       kernel_initializer=initializer, kernel_regularizer=regularizer)
+
+    # add layer 4 convolution (skip layer) to the output of the layer 7
+    # deconvolution
+    layer_4_deconv_input = tf.add(
+        layer_4_1x1_convolution, layer_7_deconvolution)
+
+    # apply deconvolution to output of layer 7 deconvolution plus layer 4 skip
+    layer_4_deconvolution = tf.layers.conv2d_transpose(layer_4_deconv_input, num_classes, 4, 2, padding='same',
+                                                       kernel_initializer=initializer, kernel_regularizer=regularizer)
+
+    # add layer 3 convolution (skip layer) to the output of the layer 4
+    # deconvolution
+    layer_3_deconv_input = tf.add(
+        layer_3_1x1_convolution, layer_4_deconvolution)
+
+    # apply deconvolution to output of the layer 4 deconvolution plus layer 3
+    # skip
+    layer_3_deconvolution = tf.layers.conv2d_transpose(layer_3_deconv_input, num_classes, 16, 8, padding='same',
+                                                       kernel_initializer=initializer, kernel_regularizer=regularizer)
+
+    return layer_3_deconvolution
+
 tests.test_layers(layers)
 
 
@@ -60,8 +115,21 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     :param num_classes: Number of classes to classify
     :return: Tuple of (logits, train_op, cross_entropy_loss)
     """
-    # TODO: Implement function
-    return None, None, None
+
+    logits = tf.reshape(nn_last_layer, (-1, num_classes))
+    labels = tf.reshape(correct_label, (-1, num_classes))
+
+    # compute softmax cross entropy between logits and labels
+    cross_entropy_loss = tf.reduce_mean(
+        tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=labels))
+
+    # use Adam optimizer to compute gradients for loss and gradients to
+    # variables in the networks
+    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+    training_operation = optimizer.minimize(cross_entropy_loss)
+
+    return logits, training_operation, cross_entropy_loss
+
 tests.test_optimize(optimize)
 
 
@@ -80,8 +148,29 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
     :param keep_prob: TF Placeholder for dropout keep probability
     :param learning_rate: TF Placeholder for learning rate
     """
-    # TODO: Implement function
-    pass
+
+    # track best loss over time for early termination
+    best_loss = sys.float_info.max
+    epochs_without_decrease = 0
+
+    for epoch in range(epochs):
+        # terminate training early if more than two epochs have passed without
+        # decreasing the loss
+        if epochs_without_decrease > 2:
+            print("Terminating search early after %s epochs" % epoch)
+            break
+        print("Running training epoch %s" % (epoch + 1))
+        loss = 0
+        for image, label in get_batches_fn(batch_size):
+            # train
+            output, loss = sess.run([train_op, cross_entropy_loss], feed_dict={
+                                    input_image: image, correct_label: label, keep_prob: 0.5, learning_rate: 0.001})
+            print("Loss on batch: %s" % loss)
+        epochs_without_decrease += 1
+        if loss < best_loss:
+            epochs_without_decrease = 0
+            best_loss = loss
+
 tests.test_train_nn(train_nn)
 
 
@@ -90,33 +179,43 @@ def run():
     image_shape = (160, 576)
     data_dir = './data'
     runs_dir = './runs'
+    epochs = 50
+    batch_size = 10
     tests.test_for_kitti_dataset(data_dir)
 
-    # Download pretrained vgg model
-    helper.maybe_download_pretrained_vgg(data_dir)
-
-    # OPTIONAL: Train and Inference on the cityscapes dataset instead of the Kitti dataset.
-    # You'll need a GPU with at least 10 teraFLOPS to train on.
-    #  https://www.cityscapes-dataset.com/
+    # VGG dataset downloadable from https://s3-us-west-1.amazonaws.com/udacity-selfdrivingcar/vgg.zip
+    # unzip in data/vgg directory
 
     with tf.Session() as sess:
         # Path to vgg model
         vgg_path = os.path.join(data_dir, 'vgg')
+
         # Create function to get batches
-        get_batches_fn = helper.gen_batch_function(os.path.join(data_dir, 'data_road/training'), image_shape)
+        get_batches_fn = helper.gen_batch_function(
+            os.path.join(data_dir, 'data_road/training'), image_shape)
 
-        # OPTIONAL: Augment Images for better results
-        #  https://datascience.stackexchange.com/questions/5224/how-to-prepare-augment-images-for-neural-network
+        # Build NN using load_vgg, layers, and optimize function
+        (image_input, keep_prob, layer3_out, layer4_out, layer7_out) = load_vgg(
+            sess, vgg_path)
 
-        # TODO: Build NN using load_vgg, layers, and optimize function
+        nn_last_tensor = layers(
+            layer3_out, layer4_out, layer7_out, num_classes)
 
-        # TODO: Train NN using the train_nn function
+        correct_label = tf.placeholder(
+            tf.float32, [None, None, None, num_classes])
+        learning_rate = tf.placeholder(tf.float32)
+        (logits, train_op, cross_entropy_loss) = optimize(
+            nn_last_tensor, correct_label, learning_rate, num_classes)
 
-        # TODO: Save inference data using helper.save_inference_samples
-        #  helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_image)
+        sess.run(tf.global_variables_initializer())
 
-        # OPTIONAL: Apply the trained model to a video
+        # Train NN using the train_nn function
+        train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_loss, image_input,
+                 correct_label, keep_prob, learning_rate)
 
+        # Save inference data using helper.save_inference_samples
+        helper.save_inference_samples(
+            runs_dir, data_dir, sess, image_shape, logits, keep_prob, image_input)
 
 if __name__ == '__main__':
     run()
